5f4ff7c921557f790d9defc6a0b4940521e467cd
LKTSHUKUROV <timur5109sh@mail.ru>
2024-01-13 21:48:55 +0300

    first commit

---
+++ second_win.py
@@ -0,0 +1,8 @@
+# напиши здесь код для второго экрана приложения
+class TestWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
